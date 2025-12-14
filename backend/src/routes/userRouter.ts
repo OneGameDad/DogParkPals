76b06ea162ddb1dcb7c0ad52e685b652efaf796e
Gregory Pellechi <greg.pellechi@gmail.com>
2025-12-14 18:29:22 +0200

    Added userRouter with createUser & getUserByEmail + tests - all pass

---
+++ backend/src/routes/userRouter.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import userController from "../controllers/userController";
+
+const router = express.Router();
+
+router.post("/users", userController.createUser);
+router.get("/users/:email", userController.getUserByEmail);
+
+export default router;

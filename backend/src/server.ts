e27aed8f9eb58624eba48ad4611bd6d1a5901454
Mark-Byrne-Codes <markbyrne477@gmail.com>
2026-01-16 20:41:03 +0000

    feature/backend_frontend_connection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import userRouter from "./routes/userRouter";
 import authRouter from "./routes/authRouter";
 import typeSafeLogger from "./utils/typeSafeLogger";
@@ -9,8 +10,16 @@ import "./jobs/autoCheckoutJob"
 const app = express();
 const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
 
+app.use(cors({
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+}));
+
 app.use(requestIdMiddleware);
 app.use(express.json());
+
 app.use(authRouter);
 app.use(userRouter);
 

99f33462df479776e843d506dff4eb6747dd59fb
Laura Guillen <laura.guillen2@gmail.com>
2026-03-17 19:08:19 +0200

    adds English terms and privacy

--- frontend/src/pages/Privacy.tsx
+++ frontend/src/pages/Privacy.tsx
@@ -1,7 +1,20 @@
+import { useTranslation } from 'react-i18next';
+
 export const Privacy = () => {
+    const { t } = useTranslation();
+
     return (
         <div>
-            <h1>Privacy Policy</h1>
+            <h1>{t('privacyPolicy.title')}</h1>
+            <p>{t('privacyPolicy.lastUpdated')}</p>
+            
+            <p>{t('privacyPolicy.intro')}</p>
+            
+            <p>{t('privacyPolicy.use')}</p>
+            
+            <p>{t('privacyPolicy.security')}</p>
+            
+            <p>{t('privacyPolicy.update')}</p>
         </div>
     );
 };
--- frontend/src/pages/Terms.tsx
+++ frontend/src/pages/Terms.tsx
@@ -1,7 +1,19 @@
+import { useTranslation } from 'react-i18next';
+
 export const Terms = () => {
+    const { t } = useTranslation();
     return (
         <div>
-            <h1>Terms and Conditions</h1>
+            <h1>{t('termsOfService.title')}</h1>
+            <p>{t('termsOfService.lastUpdated')}</p>
+            
+            <p>{t('termsOfService.intro')}</p>
+            
+            <p>{t('termsOfService.description')}</p>
+            
+            <p>{t('termsOfService.content')}</p>
+            
+            <p>{t('termsOfService.update')}</p>
         </div>
     );
 };
